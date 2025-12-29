9fdf1ec5bf9a4967c7cd3b645cac9e4e76706778
Miku <3299751760@qq.com>
2025-12-29 12:40:46 +0800

    feat: train some models

--- showcase_SAC.py
+++ showcase_SAC.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
 parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
-parser.add_argument('--ModelIdex', type=int, default=0, help='which model to load')
+parser.add_argument('--ModelIdex', type=int, default=100, help='which model to load')
 
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--Max_train_steps', type=int, default=4e5, help='Max training steps')
