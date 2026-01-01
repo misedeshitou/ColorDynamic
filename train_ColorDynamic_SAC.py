b067a79c025654adc99c059b6b2918bd15acce7a
Miku <3299751760@qq.com>
2026-01-01 20:29:25 +0800

    change default index

--- train_ColorDynamic_SAC.py
+++ train_ColorDynamic_SAC.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
 parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
-parser.add_argument('--ModelIdex', type=int, default=50, help='which model to load')
+parser.add_argument('--ModelIdex', type=int, default=2000, help='which model to load')
 
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--Max_train_steps', type=int, default=1e8, help='Max training steps')
