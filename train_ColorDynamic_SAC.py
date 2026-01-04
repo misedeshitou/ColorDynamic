1e47a40e3445b47850b57f4ce1c3ae7e359c2925
Miku <3299751760@qq.com>
2026-01-04 21:24:08 +0800

    save interval

--- train_ColorDynamic_SAC.py
+++ train_ColorDynamic_SAC.py
@@ -19,7 +19,7 @@ parser.add_argument('--ModelIdex', type=int, default=2000, help='which model to 
 
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--Max_train_steps', type=int, default=1e8, help='Max training steps')
-parser.add_argument('--save_interval', type=int, default=1, help='Model saving interval, in steps.')
+parser.add_argument('--save_interval', type=int, default=1e5, help='Model saving interval, in steps.')
 parser.add_argument('--eval_interval', type=int, default=2e3, help='Model evaluating interval, in steps.')
 parser.add_argument('--random_steps', type=int, default=1e4, help='steps for random policy to explore')
 parser.add_argument('--update_every', type=int, default=50, help='training frequency')
