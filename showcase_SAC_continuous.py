5203da34b155c2c43555ec2fa07754f2c46e14f1
Miku <3299751760@qq.com>
2026-01-06 00:04:20 +0800

    add more bad models

--- showcase_SAC_continuous.py
+++ showcase_SAC_continuous.py
@@ -15,7 +15,7 @@ parser.add_argument('--EnvIdex', type=int, default=0, help='PV1, Lch_Cv2, Humanv
 parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not') 
 parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
-parser.add_argument('--ModelIdex', type=int, default=2300, help='which model to load')
+parser.add_argument('--ModelIdex', type=int, default=2500, help='which model to load')
 
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--Max_train_steps', type=int, default=int(1e8), help='Max training steps')
